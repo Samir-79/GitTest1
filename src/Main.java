f55e7ebbad4d78e14dbdb120cafffb9a6f33fe88
Samir79 <samirovsky79@gmail.com>
2022-06-27 13:39:15 +0200

    Change 10 from Original

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
 
 
     public static void main(String[] args) {
-        System.out.println("Hello World! Change 8 and 9 combined");
+        System.out.println("Hello World! Change 10 from Original");
     }
 }
